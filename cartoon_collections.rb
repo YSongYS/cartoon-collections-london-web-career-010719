21647138fc00ac0d35557f18fc5f2803d7e2b3c1
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 23:06:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   # Your code here
-  array.each_with_index { |element| puts "#{inde+1}. #{element}" }
+  array.each_with_index { |element, index| puts "#{index+1}. #{element}" }
 end
 
 def summon_captain_planet(array)
