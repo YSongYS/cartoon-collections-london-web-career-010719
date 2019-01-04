dde3515b57bfb3788731837627ba481dcfb4ee89
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 23:01:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)
   array.collect { |element| element.capitalize<<"!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? { |element| element.size>4 }
 end
 
 def find_the_cheese# code an argument here
